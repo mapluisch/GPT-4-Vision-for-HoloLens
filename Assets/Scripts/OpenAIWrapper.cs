30e4cb11aaada6db684ea751a4365e3a8c3615e9
Martin Pluisch <31780571+mapluisch@users.noreply.github.com>
2023-11-22 10:35:49 +0100

    Added OnOpenAIRequest event + refactoring

--- Assets/Scripts/OpenAIWrapper.cs
+++ Assets/Scripts/OpenAIWrapper.cs
@@ -13,16 +13,20 @@ public class OpenAIWrapper : MonoBehaviour
     [SerializeField] private int maxTokens = 300;
     [SerializeField, Range(0f, 1f)] private float samplingTemperature = 0.5f; 
     [SerializeField] private ImageDetail imageDetail = ImageDetail.Auto;
+    
     public static event Action<string> OnOpenAIResponse;
+    public static event Action OnOpenAIRequest;
 
     public async Task<string> AnalyzeImageWithPrompt(byte[] imageData, string prompt)
     {
-        basePrompt = prompt;
-        return await AnalyzeImage(imageData);
+        // analyze image with independent prompt
+        string base64Image = Convert.ToBase64String(imageData);
+        return await MakeOpenAIRequest(base64Image, prompt);
     }
     
     public async Task<string> AnalyzeImage(byte[] imageData)
     {
+        // analyze image with specified basePrompt
         string base64Image = Convert.ToBase64String(imageData);
         return await MakeOpenAIRequest(base64Image, basePrompt);
     }
@@ -30,57 +34,57 @@ public class OpenAIWrapper : MonoBehaviour
     private async Task<string> MakeOpenAIRequest(string base64Image, string prompt)
     {
         Debug.Log("Sending new request to OpenAI.");
-        using (var httpClient = new HttpClient())
-        {
-            httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", openAIKey);
+        OnOpenAIRequest?.Invoke();
+
+        using var httpClient = new HttpClient();
+        httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", openAIKey);
             
-            // via https://platform.openai.com/docs/guides/vision
-            var payload = new
+        // via https://platform.openai.com/docs/guides/vision
+        var payload = new
+        {
+            model = "gpt-4-vision-preview",
+            messages = new object[]
             {
-                model = "gpt-4-vision-preview",
-                messages = new object[]
+                new
                 {
-                    new
+                    role = "user",
+                    content = new object[]
                     {
-                        role = "user",
-                        content = new object[]
+                        new
                         {
-                            new
+                            type = "text",
+                            text = prompt
+                        },
+                        new
+                        {
+                            type = "image_url",
+                            image_url = new
                             {
-                                type = "text",
-                                text = prompt
-                            },
-                            new
-                            {
-                                type = "image_url",
-                                image_url = new
-                                {
-                                    url = "data:image/jpeg;base64," + base64Image,
-                                    detail = imageDetail.ToString().ToLower()
-                                }
+                                url = "data:image/jpeg;base64," + base64Image,
+                                detail = imageDetail.ToString().ToLower()
                             }
                         }
                     }
-                },
-                max_tokens = maxTokens,
-                temperature = samplingTemperature
-            };
+                }
+            },
+            max_tokens = maxTokens,
+            temperature = samplingTemperature
+        };
 
-            string jsonPayload = JsonConvert.SerializeObject(payload);
+        string jsonPayload = JsonConvert.SerializeObject(payload);
 
-            var httpResponse = await httpClient.PostAsync(
-                "https://api.openai.com/v1/chat/completions", 
-                new StringContent(jsonPayload, Encoding.UTF8, "application/json"));
+        var httpResponse = await httpClient.PostAsync(
+            "https://api.openai.com/v1/chat/completions", 
+            new StringContent(jsonPayload, Encoding.UTF8, "application/json"));
 
-            string jsonResponse = await httpResponse.Content.ReadAsStringAsync();
+        string jsonResponse = await httpResponse.Content.ReadAsStringAsync();
 
-            if (httpResponse.IsSuccessStatusCode)
-            {
-                OnOpenAIResponse?.Invoke(jsonResponse);
-                return jsonResponse;
-            }
-            return "Error: " + httpResponse.StatusCode.ToString();
+        if (httpResponse.IsSuccessStatusCode)
+        {
+            OnOpenAIResponse?.Invoke(jsonResponse);
+            return jsonResponse;
         }
+        return "Error: " + httpResponse.StatusCode.ToString();
     }
 
     public static string GetResponseContent(string response)
